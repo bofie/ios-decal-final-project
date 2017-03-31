6694ad20ddb56acdb997bed792ad91322b984682
Bofan Chen <bchen1@berkeley.edu>
2017-03-31 00:57:59 -0700

    concentration

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Bofan Chen on 3/31/17.
+//  Copyright © 2017 Bofan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
